1542edd2943289d569e55a34755a598bac548134
omondi-mutuma <omondimutuma@gmail.com>
2024-09-11 17:26:42 +0300

    Implement initial file structure and commmit

---
+++ app/components/AnimatedTooltipPreview.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
+import React from "react";
+
+const people = [
+  {
+    id: 1,
+    name: "Eng. William Wandera",
+    designation: "Solar Engineer",
+    image:
+      "https://images.unsplash.com/photo-1533108165347-036ec233d7ec?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    name: "Cami estrella",
+    designation: "Head of Green Research",
+    image:
+      "https://images.unsplash.com/photo-1530785602389-07594beb8b73?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    name: "George Odhiambo",
+    designation: " Head of climate research",
+    image:
+      "https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    name: "Emily Chebet",
+    designation: "Environmental scientist",
+    image:
+      "https://images.unsplash.com/photo-1520584229410-24ebf86ad31c?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  // {
+  //   id: 5,
+  //   name: "Tyler Durden",
+  //   designation: "Solar systems Developer",
+  //   image:
+  //     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+  // },
+  // {
+  //   id: 6,
+  //   name: "Doreen Lindsey",
+  //   designation: "Mechanical engineer",
+  //   image:
+  //     "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+  // },
+];
+
+export function AnimatedTooltipPreview() {
+  return (
+    <div className="flex flex-row items-center justify-center mb-10 w-full">
+      <AnimatedTooltip items={people} />
+    </div>
+  );
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { navLinks } from "../constants/index.c";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b-2 border-gray-50">
+      <nav className="w-full h-full">
+        <ul className="w-full h-full grid grid-cols-[1fr_2fr] lg:grid-cols-[1fr_1fr_2fr_1fr_1fr] text-center">
+          {navLinks.map((link, index) => (
+            <li key={index} className={link.style}>
+              {link.isLogo ? (
+                <Image
+                  src={link.logo}
+                  alt="Company logo"
+                  width={64}
+                  height={40}
+                />
+              ) : (
+                <a className="flex justify-center items-center">{link.name}</a>
+              )}
+            </li>
+          ))}
+          <div className="flex md:hidden justify-end items-center p-4">
+            <RxHamburgerMenu className=" text-3xl text-center" />
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
